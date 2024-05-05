a79d542534e961a8833ab587472d8b83de970b4b
Ismail-Hossain-1 <107604527+Ismail-Hossain-1@users.noreply.github.com>
2024-05-06 01:07:49 +0600

    Update ProxyDNS.java

--- src/Structural/Proxy/ProxyDNS.java
+++ src/Structural/Proxy/ProxyDNS.java
@@ -7,12 +7,17 @@ public class ProxyDNS implements DNS{
 
     private DNS ActualDNS;
     private List<String> restrictedSites;
+    private List<String> adminRequired;
 
     public ProxyDNS(){
         ActualDNS = new ActualDNS();
         restrictedSites= new ArrayList<>();
         restrictedSites.add("restricted1.com");
         restrictedSites.add("restricyed2.com");
+        
+        adminRequired= new ArrayList<>();
+
+        adminRequired.add("rm-rf");
     }
 
 
@@ -24,6 +29,9 @@ public class ProxyDNS implements DNS{
             System.out.println("Access to "+server+" is restricted");
         }else {
             ActualDNS.connectTo(server);
+        }
+        if(adminRequired.contains(server)){
+            System.out.println("Admin required");
         }
 
     }
